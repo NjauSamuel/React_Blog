caeec5e9a985315559252497b7bef3b9d3d55fad
NjauSamuel <njaus602@gmail.com>
2024-08-03 18:17:44 +0300

    The new Post form can now submit a post.

--- src/pages/post/NewPost.jsx
+++ src/pages/post/NewPost.jsx
@@ -1,14 +1,103 @@
+import { useState, useRef, useEffect } from "react";
+import axios from "../../utils/axiosInstance";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import addPostValidator from "../../validators/addPostValidator";
+
+const initialFormData = {
+  title: "",
+  desc: "",
+  category: ""
+}
+const initialFormError = {
+  title: "",
+  category: ""
+}
 
 const NewPost = () => {
 
   const navigate = useNavigate()
 
+  const [formData, setFormData] = useState(initialFormData);
+  const [formError, setFormError] = useState(initialFormError);
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([])
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try{
+        // Api Request
+        const response = await axios.get(`category?size=1000`)
+        const data = response.data.data
+        setCategories(data.categories)
+        
+      }catch(error){
+        
+        const response = error.response;
+        const data = response.data;
+        toast.error(data.message, {
+          autoClose: 6000,
+        });
+      }
+    }
+
+    getCategories();
+  }, [])
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    const errors = addPostValidator({title: formData.title, category: formData.category})
+
+    if(errors.title || errors.category){
+      setFormError(errors);
+    }else{
+      try{
+        setLoading(true);
+
+        // API request
+        const requestBody = {
+          title: formData.title,
+          category: formData.category
+        }
+
+        const response = await axios.post("posts", requestBody)
+
+        const data = response.data;
+
+        toast.success(data.message, {
+          autoClose: 6000,
+        });
+
+        setFormData(initialFormData)
+        setFormError(initialFormError)
+        setLoading(false);
+        navigate("/posts")
+        
+      }catch(error){
+        setLoading(false);
+        const response = error.response;
+        const data = response.data;
+        toast.error(data.message, {
+          autoClose: 6000,
+        });
+        
+      }
+    }
+  }
+
+  console.log(formData)
+
   return (
     <div>
       <button className="button button-block" onClick={() => {navigate(-1)}}>Go Back</button>
       <div className="form-container">
-        <form className="inner-container">
+        <form className="inner-container" onSubmit={handleSubmit}>
           <h2 className="form-title">New Post</h2>
           <div className="form-group">
             <label>Title</label>
@@ -17,7 +106,10 @@ const NewPost = () => {
               type="text"
               name="title"
               placeholder="React blog post"
+              value={formData.title}
+              onChange={handleChange}
             />
+            {formError.title && <p className="error">{formError.title}</p> }
           </div>
 
           <div className="form-group">
@@ -27,6 +119,8 @@ const NewPost = () => {
               type="text"
               name="desc"
               placeholder="Lorem ipsum"
+              value={formData.desc}
+              onChange={handleChange}
             ></textarea>
           </div>
 
@@ -42,15 +136,18 @@ const NewPost = () => {
 
           <div className="form-group">
             <label>Select a category</label>
-            <select className="form-control" name="category">
-              <option value="Category 1">Category 1</option>
-              <option value="Category 2">Category 2</option>
-              <option value="Category 3">Category 3</option>
+            <select className="form-control" name="category" value={formData.category} onChange={handleChange}>
+              
+              {categories.map((category) => (
+                <option key={category._id} value={category._id}>{category.title}</option>
+              ))}
+              
             </select>
+            {formError.category && <p className="error">{formError.category}</p> }
           </div>
 
           <div className="form-group">
-            <input className="button" type="submit" value="Add" />
+            <input className="button" type="submit" value={`${loading ? "Adding..." : "Add"}`}/>
           </div>
         </form>
       </div>
---
+++ src/validators/addPostValidator.js
@@ -0,0 +1,18 @@
+const addPostValidator = ({ title, category }) => {
+    const errors = {
+        title: "",
+        category: ""
+    }
+
+    if (!title) {
+        errors.title = "Title is Required!"
+    }
+
+    if (!category) {
+        errors.category = "Category is Required!"
+    }
+
+    return errors;
+}
+
+export default addPostValidator;
