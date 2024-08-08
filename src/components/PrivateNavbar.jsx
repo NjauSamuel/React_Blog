e2c1b4633b870f71f397586e08429b5f84fb10d1
NjauSamuel <njaus602@gmail.com>
2024-08-08 13:54:17 +0300

    Implemented Authorization In React. (Hiding some buttons based on Rank Admin/Regular user. )

--- src/components/PrivateNavbar.jsx
+++ src/components/PrivateNavbar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import {useAuth} from "./context/AuthContext";
 
 const PrivateNavbar = () => {
 
     const navigate = useNavigate();
+    const auth = useAuth();
 
     const handleLogout = () => {
         window.localStorage.removeItem("blogData");
@@ -17,7 +19,9 @@ const PrivateNavbar = () => {
         <nav className="primary-link">
 
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/categories">Categories</NavLink>
+            {(auth.role === 1 || auth.role === 2) && (
+                <NavLink to="/categories">Categories</NavLink>
+            )}
             <NavLink to="/posts">Posts</NavLink>
             <NavLink to="/profile">Profile</NavLink>
             <NavLink to="/setting">Setting</NavLink>
